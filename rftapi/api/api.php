e3b56c7fc51eddab858fa1db4e398e8b84e400e1
Terre <Terre@TERREHUN>
2018-04-06 00:52:56 +0200

    Partial API Implementation  - insert 100%  - update 100%  - GET method exchange with the POST method  - supplement with Error Codes  - API user guide soon

--- rftapi/api/api.php
+++ rftapi/api/api.php
@@ -1,25 +1,47 @@
 <?php
 include_once '../core/database.php';
 
-function Error(){
+function Error($err  = '0x0'){
     $data = [
         'access'         => 'false',
-        'successful'    => 'false'
+        'successful'    => 'false',
+        'errorcode' => $err
     ];
     return json_encode($data);
 }
 
+function HalfSuccess($error = []){
+    $data = [
+        'access'         => 'true',
+        'successful'    => 'false'
+    ];
+    foreach ($error as $key => $value) {
+        $data += [''.$key.'' => ''.$value.''];
+    }
+    return json_encode($data); 
+}
+
+function Success(){
+    $data = [
+        'access'         => 'true',
+        'successful'    => 'true'
+    ];
+    return json_encode($data); 
+}
+
 function getValid($api = '',$security = ''){    
     $data =[
-        'apikey'        => $_GET['apikey'],
-        'securitykey'   => $_GET['securitykey']
+        ':apikey'        => $api        //$_GET['apikey'],
     ];
-    $g = "SELECT * FROM rft_apikey where apikey=:apikey AND securitykey=:securitykey;";
-    $exits = QL_row("SELECT * FROM rft_apikey where apikey='k2huJIqBoyNbC4tt' AND securitykey='ic5bIpGxMHTfMvNwBOY6y5xcvi5wMUHifUXeeJZzznn0D';");
-    if(!$exits)
+    //$exits = QL_row("SELECT * FROM rft_apikey WHERE apikey='k2huJIqBoyNbC4tt' AND securitykey='ic5bIpGxMHTfMvNwBOY6y5xcvi5wMUHifUXeeJZzznn0D';");
+    $exits = QL_row("SELECT * FROM rft_apikey WHERE apikey=:apikey;", $data);
+    if($exits['apikey'] == $api && $exits['securitykey'] == $security)
+        return TRUE;
+    elseif($exits['apikey'] == $api){
+        echo Error('0x2');
+        return FALSE;    
+    }else
         return FALSE;
-    else
-        return TRUE;     
 }
 
 function getFilms(){
@@ -46,11 +68,8 @@ function getFilms(){
         ];
         return json_encode($data);    
     }else{
-        $data = [
-            'access'         => 'true',
-            'successful'    => 'false'
-        ];
-        return json_encode($data); 
+        $error = ['errorcode' => '0x6'];
+        return HalfSuccess($error); 
     }
 }
 
@@ -76,11 +95,8 @@ function getBuyers(){
         ];
         return json_encode($data);
     }else{
-        $data = [
-            'access'         => 'true',
-            'successful'    => 'false'
-        ];
-        return json_encode($data); 
+        $error = ['errorcode' => '0x7'];
+        return HalfSuccess($error);  
     }
 }
 
@@ -94,7 +110,7 @@ function getBuying(){
                 'vasarlasid'        => $value['vasarlasid'],
                 'filmid'            => $value['filmid'],
                 'vevoid'            => $value['vevoid'],
-                'vasarlasideje'     => $value['vasarlasideje']
+                'vasarlasideje'     => $value['vasarlasIdeje']
             ];
             $qarray[$i] = $tmp;
             $i++;
@@ -106,14 +122,11 @@ function getBuying(){
         ];
         return json_encode($data);
     }else{
-        $data = [
-            'access'         => 'true',
-            'successful'    => 'false'
-        ];
-        return json_encode($data); 
+        $error = ['errorcode' => '0x8'];
+        return HalfSuccess($error);
     }
 }
-  
+
 function getShow(){
     $query = QL_array('SELECT * FROM rft_eloadasok');
     if($query  != NULL){
@@ -135,16 +148,13 @@ function getShow(){
         ];
         return json_encode($data);
     }else{
-        $data = [
-            'access'         => 'true',
-            'successful'    => 'false'
-        ];
-        return json_encode($data); 
+        $error = ['errorcode' => '0x9'];
+        return HalfSuccess($error); 
     }
 }
 
-function getQuery($want){
-    switch ($want){
+function getQuery($data){
+    switch ($data["want"]){
         case "filmek":{
             echo getFilms();
             return;
@@ -161,28 +171,203 @@ function getQuery($want){
             echo getBuying();
             return;
         }
+        default :{
+           $error = ['errorcode' => '0x5'];
+           echo HalfSuccess($error);
+           return;
+        }
     }
 }
 
-if(array_key_exists("apikey", $_GET) && array_key_exists("securitykey", $_GET)){
-    if(getValid($_GET['apikey'], $_GET['securitykey'])){
-        switch ($_GET['request']){
-            case "query":{
-                    getQuery($_GET['want']);
-            }break;
-            default :{
-                $data = [
-                    'access'         => 'true',
-                    'successful'    => 'false'
-                ];
-                echo json_encode($data); 
-            }break;
+function setBuyer($data){
+    $error = ['errorcode' => '0xa'];
+    if(array_key_exists("nev", $data) && array_key_exists("torzsvendeg", $data)){
+        if(executeDML("INSERT INTO rft_vevok(nev,torzsvendeg) VALUES(:nev, :torzs)", [':nev' => $data['nev'], ':torzs' => $data['torzsvendeg']]))
+            echo Success();
+        else 
+            echo HalfSuccess($error);
+    }elseif (array_key_exists("nev", $data)) {
+        if(executeDML("INSERT INTO rft_vevok(nev) VALUES(:nev)", [':nev' => $data['nev']]))
+            echo Success();
+        else 
+            echo HalfSuccess($error);
+    } else {
+        echo HalfSuccess($error);
+    }
+}
+
+function setMovies($data){
+    $error = ['errorcode' => '0xb'];
+    if(array_key_exists("cim", $data) && array_key_exists("mufaj", $data) && array_key_exists("hossz", $data) && array_key_exists("rendezo", $data) && array_key_exists("vetitike", $data)){
+        if(executeDML("INSERT INTO rft_filmek(cim,mufaj,hossz,rendezo,vetitike) VALUES(:com, :mufaj, :hossz, :rendezo, :vetitike)", 
+                [':cim' => $data['cim'], ':mufaj' => $data['mufaj'], ':hossz' => $data['hossz'], ':vetitike' => $data['vetitike']]))
+            echo Success();
+        else 
+            echo HalfSuccess($error);
+    }elseif(array_key_exists("cim", $data) && array_key_exists("mufaj", $data) && array_key_exists("hossz", $data) && array_key_exists("rendezo", $data)){
+        if(executeDML("INSERT INTO rft_filmek(cim,mufaj,hossz,rendezo,vetitike) VALUES(:com, :mufaj, :hossz, :rendezo)", 
+                [':cim' => $data['cim'], ':mufaj' => $data['mufaj'], ':hossz' => $data['hossz']]))
+            echo Success();
+        else 
+            echo HalfSuccess($error);
+    }elseif (array_key_exists("nev", $data)) {
+        if(executeDML("INSERT INTO rft_filmek(cim) VALUES(:cim)", [':cim' => $data['cim']]))
+            echo Success();
+        else 
+            echo HalfSuccess($error);
+    } else {
+        echo HalfSuccess($error);
+    }
+}
+
+function setPresentations($data){
+    $error = ['errorcode' => '0xc'];
+    if(array_key_exists("filmid", $data) && array_key_exists("idopont", $data)){
+        if(executeDML("INSERT INTO rft_eloadasok(filmid,idopont) VALUES(:fid, :idopont)", [':fid' => $data['filmid'], ':idopont' => $data['idopont']]))
+            echo Success();
+        else 
+            echo HalfSuccess($error);
+    }else {
+        echo HalfSuccess($error);
+    }
+}
+
+function setBuying($data){
+    $error = ['errorcode' => '0xd'];
+    if(array_key_exists("filmid", $data) && array_key_exists("vevoid", $data) && array_key_exists("ido", $data)){
+        if(executeDML("INSERT INTO rft_vasarlasok(filmid,vevoid,vasarlasIdeje) VALUES(:fid, :vevoid, :ido)", [':fid' => $data['filmid'], ':vevoid' => $data['vevoid'],':ido' => $data['ido']]))
+            echo Success();
+        else 
+            echo HalfSuccess($error);
+    }elseif(array_key_exists("filmid", $data) && array_key_exists("vevoid", $data)){
+        if(executeDML("INSERT INTO rft_vasarlasok(filmid,vevoid) VALUES(:fid, :vevoid)", [':fid' => $data['filmid'], ':vevoid' => $data['vevoid']]))
+            echo Success();
+        else 
+            echo HalfSuccess($error);
+    }else {
+        echo HalfSuccess($error);
+    }
+}
+
+function setInsert($data){
+    switch ($data["want"]){
+        case "buyer":{
+            setBuyer($data);
+            return;
+        }break;
+        case "movie":{
+            setMovies($data);
+            return;
+        }break;
+        case "pre":{
+            setPresentations($data);
+            return;
         }
-        return;
+        case "buying":{
+            setBuying($data);
+            return;
+        }
+        default :{
+            $error = ['errorcode' => '0x5'];
+            echo HalfSuccess($error); 
+        }
+    }
+}
+
+function updateBuyer($data){
+    $error = ['errorcode' => '0xe'];
+    if(array_key_exists("id", $data) && array_key_exists("nev", $data) && array_key_exists("torzsvendeg", $data) && array_key_exists("szam", $data)){
+        if(executeDML("UPDATE rft_vevok SET nev=:nev, torzsvendeg=:torzsvendeg, latogatasSzama=:szam WHERE id=:id", 
+                [':id' => $data['id'], ':nev' => $data['nev'],':torzsvendeg' => $data['torzsvendeg'],':szam' => $data['szam']]))
+            echo Success();
+        else 
+            echo HalfSuccess($error);
+    }else {
+        echo HalfSuccess($error);
+    }
+}
+
+function updateMovies($data){
+    $error = ['errorcode' => '0xf'];
+    if(array_key_exists("id", $data) && array_key_exists("cim", $data) && array_key_exists("hossz", $data) && array_key_exists("mufaj", $data) && array_key_exists("rendezo", $data)){
+        if(executeDML("UPDATE rft_filmek SET cim=:cim, mufaj=:mufaj, hossz=:hossz, rendezo=:rendezo WHERE id=:id", 
+                [':id' => $data['id'], ':cim' => $data['cim'],':mufaj' => $data['mufaj'],':hossz' => $data['hossz'],':rendezo' => $data['rendezo']]))
+            echo Success();
+        else 
+            echo HalfSuccess($error);
+    }elseif(array_key_exists("id", $data) && array_key_exists("cim", $data)){
+        if(executeDML("UPDATE rft_filmek SET cim=:cim WHERE id=:id", [':id' => $data['id'], ':cim' => $data['cim']]))
+            echo Success();
+        else 
+            echo HalfSuccess($error);
+    }else {
+        echo HalfSuccess($error);
+    }
+}
+
+function updatePresentations($data){
+    $error = ['errorcode' => '0x10'];
+    if(array_key_exists("id", $data) && array_key_exists("idopont", $data) && array_key_exists("filmid", $data)){
+        if(executeDML("UPDATE rft_eloadasok SET filmid=:filmid, idopont=:idopont WHERE id=:id", [':id' => $data['id'], ':filmid' => $data['filmid'],':idopont' => $data['idopont']]))
+            echo Success();
+        else 
+            echo HalfSuccess($error);
+    }else {
+        echo HalfSuccess($error);
+    }
+}
+
+function setUpdate($data){
+    switch ($data["want"]){
+        case "buyer":{
+            updateBuyer($data);
+            return;
+        }break;
+        case "movie":{
+            updateMovies($data);
+            return;
+        }break;
+        case "pre":{
+            updatePresentations($data);
+            return;
+        }
+        default :{
+            $error = ['errorcode' => '0x5'];
+            echo HalfSuccess($error); 
+        }
+    }
+}
+
+if(array_key_exists("data", $_POST)){    
+    if($_POST['data'] != NULL && $_POST['data'] != ""){
+        $data = json_decode((string)$_POST['data'], true);
+        if(getValid($data['apikey'], $data['securitykey'])){
+            switch ($data['request']){
+                case "0":{ //query
+                    getQuery($data);
+                }break;
+                case "1":{ //insert
+                    setInsert($data);
+                }break;
+                case "2":{ //updaate
+                    setUpdate($data);
+                }break;
+                case "3":{ //delete (in the near future)
+                }break;
+                default :{
+                    $error = ['errorcode' => '0x4'];
+                    echo HalfSuccess($error);
+                }break;
+            }
+            return;
+        }else{
+            echo Error('0x1');
+            return ;
+        }
     }else{
-        echo Error();
+        echo Error('0x3');
         return;
-    }  
+    }
 }
 echo Error();
 ?>
