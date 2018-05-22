1a87a39aac57d6efb5c9309063aaaf0f70b4debe
hevesi <hevesigergo6@gmail.com>
2018-05-22 22:45:08 +0200

    Kliens hozzáadva
    Helyi adatbázissal működik.
    További funkciók implementálása szükséges.

---
+++ MoziKliens/MoziKliens/Eloadas.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoziKliens
+{
+    class Eloadas : BaseQuery
+    {
+        int _filmId;
+        public int filmId
+        {
+            get { return _filmId; }
+            set { _filmId = value; }
+        }
+
+        DateTime _idopont;
+        public DateTime idopont
+        {
+            get { return _idopont; }
+            set { _idopont = value; }
+        }
+    }
+}
